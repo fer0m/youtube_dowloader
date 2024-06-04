96338d41852069a5c6a2812c584a7eea0b273c87
as <as@freezingdata.de>
2024-06-04 17:10:18 +0200

    2. Added async method to get info/download video.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from asgiref.sync import sync_to_async
 from django.http import FileResponse
 from django.shortcuts import render
 from pytube import YouTube
@@ -8,50 +9,53 @@ from hurry.filesize import size
 from .forms import Form
 from .models import Post
 
-# Create your views here.
-
 logger = logging.getLogger(__name__)
 
+from django.views.decorators.http import require_http_methods
 
-def main_site(request):
-    if request.method == 'POST':
-        form = Form(request.POST)
 
-        if 'checkForm' in request.POST:
-            if form.is_valid():
-                context = check_form(form)
-                return render(request, 'main/base_page.html', context=context)
-            else:
-                return render(request, 'main/base_page.html', context={'form': form})
+@require_http_methods(["GET", "POST"])
+async def main_site(request):
+    form = Form(request.POST)
+    if 'checkForm' in request.POST:
+        if form.is_valid():
+            context = await check_form(form)
+            return render(request, 'main/base_page.html', context=context)
+        else:
+            return render(request, 'main/base_page.html', {'form': form})
 
-        elif 'downloadForm' in request.POST:
-            if form.is_valid():
-                video = get_video(form)
-                return FileResponse(video, as_attachment=True)
-            else:
-                return render(request, 'main/base_page.html', context={'form': form})
+    elif 'downloadForm' in request.POST:
+        if form.is_valid():
+            video = await get_video(form)
+            return FileResponse(video, as_attachment=True)
+        else:
+            return render(request, 'main/base_page.html', {'VideoForm': form})
 
     else:
         form = Form()
         return render(request, 'main/base_page.html', {'form': form})
 
 
-def check_form(form):
+async def check_form(form):
     link = form.cleaned_data['text']
-    video = YouTube(link).streams.filter(progressive=True).get_highest_resolution()
-
-    video_filesize = size(video.filesize)
-    video_title = video.title
-    video_expiration = video.expiration
-    video_resolution = video.resolution
-
-    context = {'form': form, 'title': video_title,
-               'expiration': video_expiration,
-               'filesize': video_filesize,
-               'resolution': video_resolution}
+    video_info = await get_video_info(link)
+    context = {'form': form, **video_info}
     return context
 
 
+@sync_to_async
+def get_video_info(link):
+    video = YouTube(link).streams.filter(progressive=True).get_highest_resolution()
+    print(video)
+    return {
+        'filesize': size(video.filesize),
+        'title': video.title,
+        'expiration': video.expiration,
+        'resolution': video.resolution
+    }
+
+
+@sync_to_async
 def get_video(form):
     link = form.cleaned_data['text']
     post = Post()
